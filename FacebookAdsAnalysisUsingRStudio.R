d92bac6f3d2f38990ee2e2cac08c7240149638a4
evelynrofa0607 <evelyn.rodriguez@cesa.edu.co>
2022-03-22 11:44:05 -0500

    Nuevos archivos

---
+++ FacebookAdsAnalysisUsingRStudio.R
@@ -0,0 +1,8 @@
+#Load packages
+library(tidyverse)
+
+#Import the data
+data <- read_csv("/Users/evelynrodriguezfajardo/Documents/21_ENFASIS/3_MANEJO DE DATOS/KAG_conversion_data_AdsFacebook.csv")
+
+#A quick look at the data
+glimpse(data)
